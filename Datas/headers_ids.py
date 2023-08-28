5c258aeba93c73310edf56258fe075dbdf47b422
hospit <alvis.ramon@icloud.com>
2023-08-29 03:33:23 +0800

    first commit,version 0.1

---
+++ Datas/headers_ids.py
@@ -0,0 +1,24 @@
+
+
+appid_headers = {
+    "code_tenant_header": {
+        "Content-Type": "application/json;charset=utf-8"
+    }
+}
+
+appid_datas = {
+    "code_data": {
+        "appid": "",
+        "sessionid": ""
+    },
+    "appid_appsecret_data": {
+        "app_id": "",
+        "app_secret": ""
+    },
+    "appid_secret_data": {
+        "appid": "",
+        "secret": ""
+    }
+}
+
+
