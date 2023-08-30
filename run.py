622126c20915c805a0d60b9a85b5acde7c1a258a
hospit <alvis.ramon@icloud.com>
2023-08-30 12:57:22 +0800

    delete 0.2version

--- run.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import os
-
-if __name__ == "__main__":
-    # pytest.main(["-vs", "--alluredir", "./logs", "--reruns=1"])
-    pytest.main(["-vs", "./testcase/test_subscriptions/test_subscriptions.py::test_create"])
-    os.system("allure generate ./logs -o ./logs/html --clean")
